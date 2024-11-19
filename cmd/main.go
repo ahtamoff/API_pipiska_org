8a5b5b2108f04311f167727af81833ab50983e60
Ахтямов Рамиль Маратович <ahtyamovrm@sovcombank.ru>
2024-11-19 20:55:02 +0300

    init

---
+++ cmd/main.go
@@ -0,0 +1,8 @@
+package main
+
+func main(){
+	//TODO
+	//1. миграции для деплоя бд postgres
+	//2. описать интерфесы storage, service, handler
+	//3. реализовать интерфейсы, storage чтобы юзал migrator
+}
